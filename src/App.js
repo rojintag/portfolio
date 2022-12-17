88e61c76bc56b3fc5956a35ab627b93451bbd69b
rojintag <106949675+rojintag@users.noreply.github.com>
2022-12-17 00:08:11 -0800

    Adding router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './app.scss';
 import Name from './components/Name';
+import About from './components/About';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Name />
+      <BrowserRouter>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/pojects">Projects</Link>
+          <Link to="/contact">contact</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Name />} />
+          <Route path="/about" element={<About />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
