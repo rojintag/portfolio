a3780ab038444c8c5da6553df892d604b77beca8
rojintag <106949675+rojintag@users.noreply.github.com>
2022-12-17 18:43:55 -0800

    create header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './app.scss';
 import Name from './components/Name';
 import About from './components/About';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/pojects">Projects</Link>
-          <Link to="/contact">contact</Link>
-        </nav>
+       <Header />
         <Routes>
           <Route path="/" element={<Name />} />
           <Route path="/about" element={<About />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div className='header'>
-      <ul>
-        <li><a>About</a></li>
-        <li><a>Projects</a></li>
-        <li><a>Contact</a></li>
-      </ul>
-    </div>
+    <>
+      <nav className='header'>
+        <Link to="/about">About</Link>
+        <Link to="/pojects">Projects</Link>
+        <Link to="/contact">contact</Link>
+      </nav>
+    </>
   )
 }
 
