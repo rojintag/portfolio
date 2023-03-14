95a1eccd1132a9de4d188b77d2c5b54784fb6d22
rojintag <106949675+rojintag@users.noreply.github.com>
2023-03-13 23:11:05 -0700

    hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,32 +7,42 @@ import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   const [darkMode, setDarkMode] = useState(true)
+  const [hamburgerMenu, setHamburgerMenu] = useState(false)
+
+  const changeMenu = () => {
+    setHamburgerMenu(!hamburgerMenu)
+  }
+
   return (
     <>
       <nav className='header'>
         <div>
-        <ThemeContext.Consumer>
+          <ThemeContext.Consumer>
             {({ changeTheme }) => (
               <div class="switch" onClick={() => {
                 setDarkMode(!darkMode);
                 changeTheme(darkMode ? themes.light : themes.dark);
               }}>
-              <span
-            className={darkMode ? 'hidden' : 'icon'}
-          >
-            <FontAwesomeIcon icon={faMoon} />
-          </span>
-          <span
-            className={darkMode ? 'icon' : 'hidden'}
-          >
-            <FontAwesomeIcon icon={faSun} />
-          </span>
-            </div>
+                <span
+                  className={darkMode ? 'hidden' : 'icon'}
+                >
+                  <FontAwesomeIcon icon={faMoon} />
+                </span>
+                <span
+                  className={darkMode ? 'icon' : 'hidden'}
+                >
+                  <FontAwesomeIcon icon={faSun} />
+                </span>
+              </div>
             )}
           </ThemeContext.Consumer>
-         
+
         </div>
-        <div>
+        <div className={hamburgerMenu ? 'rotate' : 'menu'} onClick={changeMenu}>
+          <div className={hamburgerMenu ? 'rotate1' : 'menu-line1'}></div>
+          <div className={hamburgerMenu ? 'rotate2' : 'menu-line2'}></div>
+        </div>
+        <div className={hamburgerMenu ? 'navbar-show' : 'navbar-hide'}>
           <Link to="/">About</Link>
           <Link to="/projects">Projects</Link>
           <Link to="/contact">Contact</Link>
